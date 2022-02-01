e98a1c71eaad355088ceb7bd8de57a8a078543f1
PatrickKumer <patrick.kumer@gmail.com>
2022-02-01 16:36:06 +0100

    Added toggle buttons

--- java_script.js
+++ java_script.js
@@ -2,3 +2,8 @@ const setHeader = function(){
     let HeaderNode = document.getElementById("header")
     HeaderNode.classList.toggle("color")
 }
+
+const setBackground = function(){
+    let backgroundNode = document.getElementById("background")
+    backgroundNode.classList.toggle("color-background")
+}
